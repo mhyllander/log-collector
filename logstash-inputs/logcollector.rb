0f2ae0ea19e67a1201b210b193407155761d61a7
Magnus Hyllander <mhy@peerialism.com>
2014-08-22 08:32:16 +0200

    improved stats

--- logstash-inputs/logcollector.rb
+++ logstash-inputs/logcollector.rb
@@ -189,13 +189,13 @@ class LogStash::Inputs::LogCollector < LogStash::Inputs::Base
                 serial = msgs[2]
                 request = msgs[3]
                 @logger.debug "[log-collector] rcv msg client=#{clientid} serial=#{serial} len=#{msgs.length}"
-                @logger.info "[log-collector] start processing client=#{clientid} serial=#{serial} len=#{msgs.length}"
                 start_time = Time.now.to_f
 
                 # handle request by feeding the log events to logstash
                 batch = JSON.parse(Zlib::Inflate.inflate(request))
                 host = batch['host'].force_encoding(Encoding::UTF_8)
                 events = batch['events']
+                @logger.info "[log-collector] start processing client=#{clientid} serial=#{serial} events=#{events.length}"
                 events.each do |ev|
                   data = {
                     '@timestamp' => Time.at(ev['ts']),
@@ -211,7 +211,7 @@ class LogStash::Inputs::LogCollector < LogStash::Inputs::Base
                 @zsocket.send_strings [clientid, '', serial, ['ACK',serial,batch['n']].to_json]
                 last_msg_time = now = Time.now
                 time_spent = now.to_f-start_time
-                @logger.info "[log-collector] finished processing client=#{clientid} serial=#{serial} events=#{events.length} time=%.2fs per_event=%.2fs" % [time_spent, time_spent/events.length]
+                @logger.info "[log-collector] finished processing client=#{clientid} serial=#{serial} time=%.2fs per_second=%.2fs" % [time_spent, events.length/time_spent]
               else
                 @logger.error "[log-collector] Invalid message: #{msgs}"
               end
