8e07dca8c78381d030fbcc6ebe30fcfd7e0f9d4e
Magnus Hyllander <mhy@peerialism.com>
2015-03-31 13:37:29 +0200

    Fixed problem when a monitored dir is both an ancestor and a parent of a logfile

--- log-collector/lc/monitor.rb
+++ log-collector/lc/monitor.rb
@@ -6,7 +6,6 @@ module LogCollector
     def initialize(config,event_queue)
       @collectors = {}
       @monitors = {}
-      @parent_monitors = {}
 
       config.files.each do |path,fc|
         @collectors[path] = LogCollector::Collector.new(path,fc,event_queue)
@@ -25,14 +24,12 @@ module LogCollector
       $logger.info "cancel monitors"
       @monitors.each {|p,m| m.stop}
       @monitors.clear
-      @parent_monitors.each {|p,m| m.stop}
-      @parent_monitors.clear
     end
 
     def setup_monitors
       $logger.info "setup monitors"
       logdirs = {}
-      parentdirs = {}
+      ancestordirs = {}
 
       @collectors.each do |path,collector|
         pn = Pathname.new(path)
@@ -42,46 +39,70 @@ module LogCollector
         logdirs[dir] ||= {}
         logdirs[dir][base] = collector
 
-        # get all parents of log dirs
+        # get all ancestors of log dirs
         pn = pn.parent
         until pn.root?
           dir, base = pn.split.map {|f| f.to_s}
-          parentdirs[dir] ||= {}
-          parentdirs[dir][base] ||= []
-          parentdirs[dir][base] << collector
+          ancestordirs[dir] ||= {}
+          ancestordirs[dir][base] ||= []
+          ancestordirs[dir][base] << collector
           pn = pn.parent
         end
       end
 
-      # start monitors on log dirs
-      logdirs.each do |dir,files|
-        @monitors[dir] = monitor_files(dir,files)
-      end
-
-      # start monitors on all parents of log dirs
-      parentdirs.each do |dir,parents|
-        @parent_monitors[dir] = monitor_parents(dir,parents)
+      # start monitors
+      [logdirs.keys, ancestordirs.keys].flatten.uniq.each do |dir|
+        @monitors[dir] = start_monitor(dir, logdirs[dir], ancestordirs[dir])
       end
     end
 
     # inotify events:
-    # file created:  created + modified
-    # file deleted:  deleted
-    # file modified: modified
-    # file renamed:  renamed
-    def monitor_files(dir,files)
+    # entry created:  created + modified
+    # entry deleted:  deleted
+    # entry modified: modified
+    # entry renamed:  renamed
+    def start_monitor(dir,files,ancestors_in_dir)
+      $logger.debug { %Q[start_monitor: #{dir} files=#{files} ancestors_in_dir=#{ancestors_in_dir}] }
       monitor = JRubyNotify::Notify.new
-      monitor.watch(dir, JRubyNotify::FILE_ANY, false) do |change, path, file, newfile|
+      monitor.watch(dir, JRubyNotify::FILE_ANY, false) do |change, path, entry, newentry|
         Thread.current['name'] = 'monitor'
         Thread.current.priority = 3
         begin
-          unless file =~ /\/$/
-            $logger.debug { %Q[#{path}: detected "#{file}" '#{change}' (#{newfile})] }
-            if (collector = files[file])
-              collector.notify change
-            elsif change==:renamed && (collector = files[newfile])
-              collector.notify :replaced
+          $logger.debug { %Q[#{path}: detected '#{change}' "#{entry}" (#{newentry})] }
+          # handle events on logfiles in dir
+          if files
+            unless entry =~ /\/$/
+              if (collector = files[entry])
+                collector.notify change
+              elsif change==:renamed && (collector = files[newentry])
+                collector.notify :replaced
+              end
             end
+          end
+          # handle events on ancestors (of logfiles) in dir
+          if ancestors_in_dir
+            case change
+            when :created
+              if (collectors = ancestors_in_dir[entry])
+                # base was re-created
+                check_and_restart_monitors collectors
+              end
+            when :renamed
+              # base was renamed, or another dir was renamed to base
+              if (collectors = ancestors_in_dir[entry])       # like a delete
+                # if an ancestor was deleted we just keep on reading the open file
+                # and monitor for the re-creation of the ancestor/directory
+              elsif (collectors = ancestors_in_dir[newentry]) # like a create
+                # base was re-created
+                check_and_restart_monitors collectors
+              end
+            when :deleted
+              # base was deleted
+              if (collectors = ancestors_in_dir[entry])
+                # if an ancestor was deleted we just keep on reading the open file
+                # and monitor for the re-creation of the ancestor/directory
+              end
+            end # case change
           end
         rescue OutOfMemoryError
           abort "Monitor: exiting because of java.lang.OutOfMemoryError"
@@ -90,45 +111,7 @@ module LogCollector
         end
       end # monitor.watch
       monitor.run
-      $logger.info { %Q[watching "#{dir}" for notifications about files #{files.keys}] }
-      monitor
-    end
-
-    def monitor_parents(dir,parents)
-      monitor = JRubyNotify::Notify.new
-      monitor.watch(dir, JRubyNotify::FILE_ANY, false) do |change, path, file, newfile|
-        begin
-          $logger.debug { %Q[#{path}: detected "#{file}" '#{change}' (#{newfile})] }
-          case change
-          when :created
-            if (collectors = parents[file])
-              # base was re-created
-              check_and_restart_monitors collectors
-            end
-          when :renamed
-            # base was renamed, or another dir was renamed to base
-            if (collectors = parents[file])       # like a delete
-              # if a parent was deleted we just keep on reading the open file
-              # and monitor for the re-creation of the parent/directory
-            elsif (collectors = parents[newfile]) # like a create
-              # base was re-created
-              check_and_restart_monitors collectors
-            end
-          when :deleted
-            # base was deleted
-            if (collectors = parents[file])
-              # if a parent was deleted we just keep on reading the open file
-              # and monitor for the re-creation of the parent/directory
-            end
-          end # case change
-        rescue OutOfMemoryError
-          abort "Monitor: exiting because of java.lang.OutOfMemoryError"
-        rescue Exception=>e
-          on_exception e, false
-        end
-      end # monitor.watch
-      monitor.run
-      $logger.info { %Q[watching "#{dir}" for notifications about parents #{parents.keys}] }
+      $logger.info { %Q[watching "#{dir}" for notifications about files #{files ? files.keys : []} ancestors_in_dir=#{ancestors_in_dir ? ancestors_in_dir.keys : []}] }
       monitor
     end
 
